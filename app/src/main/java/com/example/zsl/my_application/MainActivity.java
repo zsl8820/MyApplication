bdc4e5106f54ff154e58676b5302aefe46afc175
zsl <zsl@126.com>
2016-07-22 11:11:01 +0800

    My_Application

---
+++ app/src/main/java/com/example/zsl/my_application/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.zsl.my_application;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.sax.StartElementListener;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.EventLogTags;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: 执行了");
+        Button button=(Button) findViewById(R.id.activity_main_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Activitysec.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: 执行了");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+}
